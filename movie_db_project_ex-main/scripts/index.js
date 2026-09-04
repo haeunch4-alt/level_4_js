01bb73f929dc01a86772792ac244de6a73eeb041
haeunch4-alt <haeun.ch4@gmail.com>
2026-09-04 10:55:13 +0900

    swiper

--- movie_db_project_ex-main/scripts/index.js
+++ movie_db_project_ex-main/scripts/index.js
@@ -31,3 +31,40 @@ for(let i=0; i<10; i++){
     movieLi.innerHTML += `<a href="#"><img src="${movie2[i].poster}" alt="${movie2[i].title}"></a>`;
     movieWrap2.appendChild(movieLi);
 }
+
+// ------------------------------------------26/9/4
+// 무비차트 + swiper
+// swiper-slide 반복 생성해서 swiper-wrapper 붙여넣기
+// swiper 플러그인 함수 연결은 swiper 최종연결
+const chart_swiper = document.querySelector('.chart_swiper');
+const anime_swiper = document.querySelector('.anime_swiper')
+
+const chart_swiper_func = new Swiper(chart_swiper,{
+    slidesPerView:2,
+    spaceBtween:10,
+
+}); // 플러그인 연결 
+
+for(let i=0; i<=5; i++){
+    const chart_slide = document.createElement('div'); //생성
+    chart_slide.classList.add('swiper-slide'); // 클래스연결s
+
+    chart_slide.innerHTML = `<p class="num">${moviesDB[i].id}위</p>`;
+    chart_slide.innerHTML += `<h3>${moviesDB[i].title}</h3>`;
+    chart_slide.innerHTML += `<p class="rating">${moviesDB[i].rating}</p>`
+    chart_slide.innerHTML += `<p class="story">${moviesDB[i].synopsis}</p>`
+    chart_slide.style.backgroundImage = `url(${moviesDB[i].poster})`
+
+    chart_swiper.children[0].append(chart_slide); // 붙여넣기
+}
+for(let i=0; i<=8; i++){
+    const anime_slide = document.createElement('div');
+    anime_slide.classList.add('swiper-slide');
+
+    anime_slide.innerHTML = `<h3 class="title">${movie2[i].title}</h3>`;
+    anime_slide.innerHTML += `<p>${movie2[i].director}</p>`;
+    anime_slide.innerHTML += `<p>${movie2[i].cast}</p>`;
+    anime_slide.style.backgroundImage = `url(${movie2[i].poster})`
+
+    anime_swiper.children[0].append(anime_slide);
+}
